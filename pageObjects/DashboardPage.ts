0d4c0b9e60c8a035a0faabfb87c6853a967d1381
Palash soni <palash.soni@wingify.com>
2023-06-01 20:53:51 +0530

    updated

--- pageObjects/DashboardPage.ts
+++ pageObjects/DashboardPage.ts
@@ -1,28 +1,48 @@
-import{Locator,Page} from '@playwright/test';
+import { Locator, Page } from "@playwright/test";
 
-export class DashboardPage{
+export class DashboardPage {
+  readonly page: Page;
+  readonly title: Promise<string>;
+  readonly pageText: Locator;
+  readonly mobileMenu: Locator;
+  readonly productPrice: Locator;
 
-    readonly page:Page;
-    readonly title: Promise<string>;    
-    readonly pageText: Locator;    
-    readonly mobileMenu: Locator;
+  constructor(page: Page) {
+    this.page = page;
+    this.title = page.title();
+    this.mobileMenu = page.locator("li[class*='level0 nav-1']>a");
+    this.pageText = page.locator("h2");
+    this.productPrice = page.locator("span[id='product-price-1']>span");
+  }
 
-    constructor(page:Page) {
-        this.page = page;
-        this.title =  page.title();
-        this.mobileMenu = page.locator("li[class*='level0 nav-1']>a");
-        this.pageText = page.locator("h2");
-    }
+  async getTitle() {
+    return await this.title;
+  }
 
-    async getTitle(){
-        return await this.title;
-    }
+  async getPageText() {
+    return await this.pageText.textContent();
+  }
 
-    async getPageText(){
-        return await this.pageText.textContent();
-    }
+  async clickOnMobilePage() {
+    await this.mobileMenu.click();
+  }
 
-    async clickOnMobilePage(){
-        await this.mobileMenu.click();
-    }
+/*   async getSonyXperiaProductPrice():Promise<void> {
+    let hello =await this.productPrice.textContent();
+    parseFloat(hello?.replace("$", ""));
+  }
+
+  async getSonyXperiaProductPrices(): Promise<number> {
+    let hello = await this.productPrice.textContent();
+    return parseFloat(hello?.replace("$", ""));
+  }
+  async getSonyXperiaProductPricess(): Promise<number> {
+    const hello = await this.productPrice.textContent();
+    return parseFloat(hello?.replace("$", ""));
+  } */
+  async getSonyXperiaProductPrice(): Promise<number> {
+    let hello = await this.productPrice.textContent();
+    const price = parseFloat(hello!.replace("$", ""));
+    return price;
+  }
 }
--- tests/TestScripts.spec.ts
+++ tests/TestScripts.spec.ts
@@ -2,20 +2,23 @@ import { test, expect } from "@playwright/test";
 import { DashboardPage } from "../pageObjects/DashboardPage";
 import { MobilePage } from "../pageObjects/MobilePage";
 
-test.beforeEach(async ({page}) => {
+test.beforeEach(async ({ page }) => {
   await page.goto("http://live.techpanda.org/index.php/");
 });
 
-test("Test Case 1", async ({page})=> {
-    const dashboardPageObj = new DashboardPage(page);
-    expect(await dashboardPageObj.getTitle()).toEqual("Home page");
-    console.log(await dashboardPageObj.getPageText());
-    expect(await dashboardPageObj.getPageText()).toContain("demo")    
-    await dashboardPageObj.clickOnMobilePage();
+test("Test Case 1", async ({ page }) => {
+  const dashboardPageObj = new DashboardPage(page);
+  expect(await dashboardPageObj.getTitle()).toEqual("Home page");
+  console.log(await dashboardPageObj.getPageText());
+  expect(await dashboardPageObj.getPageText()).toContain("demo");
+  await dashboardPageObj.clickOnMobilePage();
 
-    const mobilePageObj = new MobilePage(page);
-    expect(await mobilePageObj.getTitle()).toEqual("Mobile");
-    expect(await mobilePageObj.isProductSortedByName()).toBeTruthy();
-})
-
-
+  const mobilePageObj = new MobilePage(page);
+  expect(await mobilePageObj.getTitle()).toEqual("Mobile");
+  expect(await mobilePageObj.isProductSortedByName()).toBeTruthy();
+});
+test.only("test case 2", async ({page}) => {
+  const dashboardPageObj = new DashboardPage(page);
+  await dashboardPageObj.clickOnMobilePage();
+  console.log(await dashboardPageObj.getSonyXperiaProductPrice());
+});
